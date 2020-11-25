bcc1c30e6ff1683cee183aa696d8d44436b2ab7f
e-coli <37364150+escherichia-coli@users.noreply.github.com>
2020-11-25 11:54:22 +0100

    Removed useless files

--- Archives/file_dialog_open_nmodel.py
+++
@@ -1,7 +0,0 @@
-from file_dialog_main import App
-
-class UI_OpenNMODEL(App):
-    def __init__(self):
-        super().__init__()
-        self.title = 'Open nmodel'
-        self.extension = "(*.nmodel)"
