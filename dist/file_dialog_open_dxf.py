fcc8007a484fac0c744b0aa666df61232bd2bde9
e-coli <37364150+escherichia-coli@users.noreply.github.com>
2020-11-25 11:41:38 +0100

    Ignoring some files

--- dist/file_dialog_open_dxf.py
+++
@@ -1,7 +0,0 @@
-from file_dialog_main import App
-
-class UI_OpenDXF(App):
-    def __init__(self):
-        super().__init__()
-        self.title = 'Open dxf'
-        self.extension = "(*.dxf)"
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Start main.py
+
+App under progress, a lot of bugs to be fixed
